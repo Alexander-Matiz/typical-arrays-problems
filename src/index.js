fabbfb10123d75593dad56a5ddcedf7ffed49da7
Alexander Matiz <alexander.matiz.general@gmail.com>
2020-10-03 14:56:35 +0300

    feat: add avg function

--- src/index.js
+++ src/index.js
@@ -32,5 +32,16 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
+  if (array && array.length > 0) {
+    let sum = 0;
+    let avg;
+
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+
+    return avg = sum / array.length;
+  }
+
   return 0;
 }
