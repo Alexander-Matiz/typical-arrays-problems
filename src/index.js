76056350b13f711c0f7d741f4e86adae0291b393
Alexander Matiz <alexander.matiz.general@gmail.com>
2020-10-03 14:55:51 +0300

    feat: add min function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 
 exports.min = function min (array) {
+  if (array && array.length > 0) {
+    let min = array[0];
+
+    for (let i = 0; i <= array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
+      }
+    }
+
+    return min;
+  }
+
   return 0;
 }
 
